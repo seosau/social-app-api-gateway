e769108fcc0e6edb6379398bcda7f5f160896663
seosau <seosausau@gmail.com>
2025-06-01 22:54:15 +0700

    commented rabbitmq

--- src/config/rabbitMQ/rabbitMQ.module.ts
+++ src/config/rabbitMQ/rabbitMQ.module.ts
@@ -1,23 +1,23 @@
-import { Module, Global } from '@nestjs/common';
-import { ClientProxyFactory, Transport, ClientProxy } from '@nestjs/microservices';
+// import { Module, Global } from '@nestjs/common';
+// import { ClientProxyFactory, Transport, ClientProxy } from '@nestjs/microservices';
 
-@Global()
-@Module({
-  providers: [
-    {
-      provide: 'RABBITMQ_SERVICE',
-      useFactory: () => {
-        return ClientProxyFactory.create({
-          transport: Transport.RMQ,
-          options: {
-            urls: ['amqp://guest:guest@rabbitmq:5672'],
-            queue: 'cats_queue',
-            queueOptions: { durable: false },
-          },
-        });
-      },
-    },
-  ],
-  exports: ['RABBITMQ_SERVICE'],
-})
-export class RabbitMQModule {}
+// @Global()
+// @Module({
+//   providers: [
+//     {
+//       provide: 'RABBITMQ_SERVICE',
+//       useFactory: () => {
+//         return ClientProxyFactory.create({
+//           transport: Transport.RMQ,
+//           options: {
+//             urls: ['amqp://guest:guest@rabbitmq:5672'],
+//             queue: 'cats_queue',
+//             queueOptions: { durable: false },
+//           },
+//         });
+//       },
+//     },
+//   ],
+//   exports: ['RABBITMQ_SERVICE'],
+// })
+// export class RabbitMQModule {}
